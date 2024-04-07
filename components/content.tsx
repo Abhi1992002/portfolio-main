691422874a72048f9ee9d1378a30d5ca21c5d947
Abhimanyu Yadav <122007096+Abhi1992002@users.noreply.github.com>
2024-04-07 17:48:00 +0530

    Update content.tsx

--- components/content.tsx
+++ components/content.tsx
@@ -24,10 +24,10 @@ export const Content = ({}: ContentProps) => {
             Let's explore what we can build together.
           </h2>
           <div className="w-full flex justify-center gap-8 mt-4 xl:mt-8">
-            <Link href={"/"}>
+            <Link href={"https://github.com/Abhi1992002"}>
               <FaGithub className="w-8 h-8 text-zinc-400 hover:text-white" />
             </Link>
-            <Link href={"/"}>
+            <Link href={"https://twitter.com/abhiY2045"}>
               <RiTwitterXLine className="w-8 h-8 text-zinc-400  hover:text-white" />
             </Link>
           </div>
